ccae95ed521b0af2c8218b992f6ed1d6a5f872be
Rida fd <ridafakherlden@gmail.com>
2021-01-11 13:18:39 +0200

    add tailwind

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- css --->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+        <!-- bulma cdn --->
+{{--        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.1/css/bulma.min.css">--}}
+
+        <!-- Fonts -->
+
+        <!-- Styles -->
+        <style>
+
+        </style>
+
+        <style>
+            body {
+                font-family: 'Nunito';
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center sm:pt-0">
+            @if (Route::has('login'))
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                    @auth
+                        <a href="{{ url('/home') }}" class="text-sm text-gray-700 underline">Home</a>
+                    @else
+                        <a href="{{ route('login') }}" class="text-sm text-gray-700 underline">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+            <h1 class="bg-red-500">welcome</h1>
+        </div>
+    </body>
+</html>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    purge: [],
+    purge: [
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+        backgroundColor: true,
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
